905be7781b1a8fa770fa48b6696463ada73e1f6c
JhyunJung <jhyun.rigel@gmail.com>
2022-01-15 21:49:37 +0900

    5장 완료

--- src/main/java/com/jhyun/TIL/springboot/web/IndexController.java
+++ src/main/java/com/jhyun/TIL/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.jhyun.TIL.springboot.web;
 
+import com.jhyun.TIL.springboot.config.auth.LoginUser;
+import com.jhyun.TIL.springboot.config.auth.dto.SessionUser;
 import com.jhyun.TIL.springboot.service.PostService;
 import com.jhyun.TIL.springboot.web.dto.PostsResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
@@ -15,8 +19,12 @@ public class IndexController {
     private final PostService postService;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postService.findAllDesc());
+
+        if(user != null){
+            model.addAttribute("userName", user.getName());
+        }
 
         return "index";
     }
